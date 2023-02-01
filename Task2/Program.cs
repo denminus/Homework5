14cdee7a59ac97517fb3188dee108d4db954db1f
den_minus <den.minus@mail.ru>
2023-02-01 11:27:08 +0300

    Числа фибоначи

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа Фибоначчи
+double Fibonacci(int n)
+{
+if(n == 1 || n == 2) return 1;
+else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 15; i++)
+{
+Console.WriteLine(Fibonacci(i));
+}
